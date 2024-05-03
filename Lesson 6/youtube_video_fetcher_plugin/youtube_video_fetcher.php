d08df586f58e6330b415bd00a62bf647d2c102bd
ecations <51368173+ecations@users.noreply.github.com>
2024-05-03 22:49:31 +0300

    apis

---
+++ Lesson 6/youtube_video_fetcher_plugin/youtube_video_fetcher.php
@@ -0,0 +1,8 @@
+<?php 
+/* 
+Plugin Name: Youtube Video Fetcher
+Description: Fetches the latest videos from a youtube channel and stores them in a database and allows the user to display them using a shortcode
+Version 0.1
+Author: Antti Perälä
+
+*/
